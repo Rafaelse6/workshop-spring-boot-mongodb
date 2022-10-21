47ab41bc4247ff9b35acf71d640faad66b8e9994
Rafael Santos <rafa.05152@gmail.com>
2022-10-21 13:49:04 -0300

    Projeto criado

---
+++ src/test/java/com/rafaelsantos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
